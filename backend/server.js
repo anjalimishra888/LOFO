c0e4edbf962b41f25c53e2b4d8fc465a15bf1018
Task-manager <Task-manager>
2026-06-19 17:40:34 +0530

    Bug fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://lofo-blu.vercel.app",
+      "https://lofo-blue.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
